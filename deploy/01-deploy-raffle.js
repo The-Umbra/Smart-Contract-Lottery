e42cfed3e43a7d0f9cf80423efb02213c0c2f046
Furkan <gami.furkan@gmail.com>
2022-10-26 17:49:37 +0300

    subscription id done

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,22 +1,30 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../heplper-hardhat-config")
 
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("30")
+
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
-    let VRFCoordinatorV2Address
+    let VRFCoordinatorV2Address, subscriptionId
 
     if (developmentChains.includes(network.name)) {
         const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
         VRFCoordinatorV2Address = VRFCoordinatorV2Mock.address
+        const transactionResponse = await VRFCoordinatorV2Mock.createSubscription()
+        const transactionReceipt = await transactionResponse.wait(1)
+        subscriptionId = transactionReceipt.events[0].args.subId
+        // Fund subscription
+        await VRFCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
     } else {
         VRFCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+        subscriptionId = networkConfig[chainId]["subscriptionId"]
     }
 
     const entranceFee = networkConfig[chainId]["entranceFee"]
     const gasLane = networkConfig[chainId]["gasLane"]
-    const args = [VRFCoordinatorV2Address, entranceFee, gasLane]
+    const args = [VRFCoordinatorV2Address, entranceFee, gasLane, subscriptionId]
     const raffle = await deploy("Raffle", {
         from: deployer,
         args: args,
